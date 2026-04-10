3724514a0965cef48925d10cb755cbdc4374bdde
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2026-04-10 13:31:12 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let value = 0
 basic.forever(function () {
-    value = pins.analogReadPin(AnalogPin.P0)
+    value = pins.analogReadPin(AnalogReadWritePin.P1)
     basic.showNumber(value)
-    basic.pause(300)
 })
